516401841f57e007a6b9cb2de7a74d9ffe76efb2
Christopher McKee <chris.mckee.13@gmail.com>
2020-01-30 20:27:13 -0800

    fixes sql linting

--- models/schema.sql
+++ models/schema.sql
@@ -2,8 +2,9 @@ DROP DATABASE IF EXISTS game_db;
 CREATE DATABASE game_db;
 USE game_db;
 
-CREATE TABLE locations (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE locations
+(
+  id INT NOT NULL,
   x INT NOT NULL,
   y INT NOT NULL,
   name VARCHAR(50) NOT NULL,
@@ -14,8 +15,9 @@ CREATE TABLE locations (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE monsters (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE monsters
+(
+  id INT NOT NULL,
   name VARCHAR(50) NOT NULL,
   hp INT NOT NULL,
   attack INT NOT NULL,
@@ -23,8 +25,9 @@ CREATE TABLE monsters (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE treasure (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE treasure
+(
+  id INT NOT NULL,
   name VARCHAR(50) NOT NULL,
   amount INT NOT NULL,
   location_id INT,
@@ -32,37 +35,48 @@ CREATE TABLE treasure (
   PRIMARY KEY (id)
 );
 
-INSERT INTO locations (x, y, name)
-VALUES 	(1, 1, "Swamp"),
-		(2, 1, "Field"),
-		(3, 1, "Abandoned Fort"),
-		(1, 2, "Forest"),
-		(2, 2, "Town"),
-		(3, 2, "Farm"),
-		(1, 3, "Dark Cave"),
-		(2, 3, "Rolling Hills"),
-		(3, 3, "Mountains");
+INSERT INTO locations
+  (x, y, name)
+VALUES
+  (1, 1, "Swamp"),
+  (2, 1, "Field"),
+  (3, 1, "Abandoned Fort"),
+  (1, 2, "Forest"),
+  (2, 2, "Town"),
+  (3, 2, "Farm"),
+  (1, 3, "Dark Cave"),
+  (2, 3, "Rolling Hills"),
+  (3, 3, "Mountains");
 
 UPDATE locations SET exit_n = FALSE WHERE y = 3;
 UPDATE locations SET exit_e = FALSE WHERE x = 3;
 UPDATE locations SET exit_w = FALSE WHERE x = 1;
 UPDATE locations SET exit_s = FALSE WHERE y = 1;
 
-INSERT INTO monsters (name, hp, attack, location_id)
-VALUES 	("Troll", 10, 3, 1),
-		("Goblin", 3, 1, 8),
-		("Owlbear", 6, 2, 4),
-		("Bandit", 5, 2, 3);
-        
-INSERT INTO treasure (name, amount, location_id)
-VALUES 	("Emerald Knife", 20, 7),
-		("Jeweled Crown", 50, 9);
-        
-INSERT INTO treasure (name, amount, monster_id)
-VALUES 	("Bag of Copper Coins", 5, 2),
-		("Silver Chalice", 40, 1),
-		("Chest of loot", 100, 4);
-        
-SELECT * FROM locations;
-SELECT * FROM monsters;
-SELECT * FROM treasure;
+INSERT INTO monsters
+  (name, hp, attack, location_id)
+VALUES
+  ("Troll", 10, 3, 1),
+  ("Goblin", 3, 1, 8),
+  ("Owlbear", 6, 2, 4),
+  ("Bandit", 5, 2, 3);
+
+INSERT INTO treasure
+  (name, amount, location_id)
+VALUES
+  ("Emerald Knife", 20, 7),
+  ("Jeweled Crown", 50, 9);
+
+INSERT INTO treasure
+  (name, amount, monster_id)
+VALUES
+  ("Bag of Copper Coins", 5, 2),
+  ("Silver Chalice", 40, 1),
+  ("Chest of loot", 100, 4);
+
+SELECT *
+FROM locations;
+SELECT *
+FROM monsters;
+SELECT *
+FROM treasure;
